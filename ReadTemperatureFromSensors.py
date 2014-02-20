5623712e888c26a9a54e395c8c91720b543f16bf
mattias.norell <mattias.norell@ninetech.se>
2014-02-20 15:52:38 +0100

    First commit.

---
+++ ReadTemperatureFromSensors.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+import MySQLdb
+
+db = MySQLdb.connect("localhost", "rpiuser", "devpassword", "RPiDev")
+curs=db.cursor()
+
+def parseTemperature( input ):
+    secondline = input.split("\n")[1]
+    temperaturedata = secondline.split(" ")[9]
+    temperature = float(temperaturedata[2:])
+    return temperature / 1000   
+
+def getSensorConfigFile (sensorId):
+    sensorPath = "/sys/bus/w1/devices/"+ sensorId +"/w1_slave"
+    tfile = open(sensorPath)
+    text = tfile.read()
+    tfile.close()
+    return text
+
+def saveLog(type, description):
+    query = """INSERT INTO Logs (lType,lDescription,lDateTime) VALUES(%s,%s,NOW())"""
+    curs.execute (query,(type, description))
+
+def checkTemperature(sensorName, temp, sensorMinValue, sensorMaxValue):
+    if temp < sensorMinValue:
+        print "To cold";
+
+    if temp > sensorMaxValue:
+        print "To hot";
+    
+def saveTemperature(sensorId, sensorName, temp):
+    query = """INSERT INTO Temperatures (tSensorId,tDatetime,tTemp) VALUES(%s,NOW(),%s)"""
+    curs.execute (query,(sensorId, temp))
+
+def readSensors():
+    curs.execute ("""SELECT * from Sensors""")
+
+    for reading in curs.fetchall():
+        sensorId = reading[0]
+        sensorSerialNumber = str(reading[1])
+        sensorName = str(reading[2])
+        sensorType = str(reading[3])
+        sensorMinValue = reading[4]
+        sensorMaxValue = reading[5]
+        text = getSensorConfigFile(sensorSerialNumber)  
+        temp = parseTemperature(text)
+
+        with db:
+            if sensorType == "temp":
+                print "Sensor: ", sensorName
+                print "Temperature: ", temp, "°C\n"
+                saveTemperature(sensorId, sensorName, temp)
+                checkTemperature(sensorName, temp, sensorMinValue, sensorMaxValue)
+                
+readSensors()
+
+db.close()
